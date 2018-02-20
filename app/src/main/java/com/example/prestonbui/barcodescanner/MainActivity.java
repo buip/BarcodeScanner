bccfe94e709187a5a93f4c33b5492f074389cce5
buip <prestonbui97@gmail.com>
2018-02-19 23:04:04 -0600

    Simple barcode check example. Worked!

--- app/src/main/java/com/example/prestonbui/barcodescanner/MainActivity.java
+++ app/src/main/java/com/example/prestonbui/barcodescanner/MainActivity.java
@@ -1,15 +1,45 @@
 package com.example.prestonbui.barcodescanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
 public class MainActivity extends AppCompatActivity {
     TextView barCodeResult;
+    Button scanBarCode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         barCodeResult = findViewById(R.id.barcode_result);
+        scanBarCode = findViewById(R.id.scan_button);
+
+        scanBarCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ScanBarCodeActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra("barcode");
+                    barCodeResult.setText("Barcode value: " + barcode.displayValue);
+                } else {
+                    barCodeResult.setText("No barcode found");
+                }
+            }
+        }
     }
 }
